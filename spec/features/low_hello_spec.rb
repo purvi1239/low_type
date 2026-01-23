b919d6e6dc4b1d4cdabd9b165eaaec2fa00db1a6
maedi <maediprichard@gmail.com>
2026-01-23 12:08:16 +1100

    Split out return types

--- spec/features/low_hello_spec.rb
+++ spec/features/low_hello_spec.rb
@@ -22,11 +22,7 @@ RSpec.describe LowHello do
         :multiple_typed_args,
         :multiple_typed_args_and_default_value,
         :typed_array_arg,
-        :return_type,
-        :array_return_type,
-        :arg_and_return_type,
-        :arg_and_nilable_return_value,
-        :private_typed_arg,
+        :private_typed_arg
       )
     end
   end
@@ -299,72 +295,6 @@ RSpec.describe LowHello do
       end
     end
   end
-
-  # Return types.
-
-  describe '#return_type' do
-    it 'returns a value' do
-      expect(hello.return_type).to eq(4)
-    end
-
-    it 'defines return type expression' do
-      hello.return_type
-      expect(described_class.low_methods[:return_type].return_proxy.type_expression.types).to eq([Integer])
-    end
-  end
-
-  describe '#array_return_type' do
-    it 'returns an array of symbols' do
-      expect(hello.array_return_type).to eq(%i[one two three])
-    end
-
-    it 'defines Array[Symmbol] return type expression' do
-      hello.array_return_type
-      expect(described_class.low_methods[:array_return_type].return_proxy.type_expression.types).to eq([Array[Symbol]])
-    end
-  end
-
-  describe '#arg_and_return_type' do
-    it 'defines return type expression' do
-      hello.arg_and_return_type('Morning')
-      expect(described_class.low_methods[:arg_and_return_type].return_proxy.type_expression.types).to eq([String])
-    end
-
-    context 'when the return value is nil' do
-      let(:error_message) { "Invalid return type 'NilClass' for method 'arg_and_return_type'. Valid types: 'String'" }
-
-      it 'raises a return type error' do
-        expect { hello.arg_and_return_type(nil) }.to raise_error(LowType::ReturnTypeError, error_message)
-      end
-    end
-
-    context 'when the return value does not validate the return type expression' do
-      let(:error_message) { "Invalid return type 'Integer' for method 'arg_and_return_type'. Valid types: 'String'" }
-
-      it 'raises a return type error' do
-        expect { hello.arg_and_return_type(123) }.to raise_error(LowType::ReturnTypeError, error_message)
-      end
-    end
-  end
-
-  describe '#arg_and_nilable_return_value' do
-    it 'defines return type expression' do
-      expect(hello.arg_and_nilable_return_value(nil)).to eq(nil)
-      expect(described_class.low_methods[:arg_and_nilable_return_value].return_proxy.type_expression.types).to eq([String])
-    end
-
-    context 'when the return value does not validate the return type expression' do
-      let(:error_message) do
-        "Invalid return type 'Integer' for method 'arg_and_nilable_return_value'. Valid types: 'String | nil'"
-      end
-
-      it 'raises a return type error' do
-        expect { hello.arg_and_nilable_return_value(123) }.to raise_error(LowType::ReturnTypeError, error_message)
-      end
-    end
-  end
-
-  # TODO: Return type that is literally a type (should probably pass). Test both basic type and complex type.
 
   describe '#private_typed_arg' do
     let(:error_message) { "private method 'private_typed_arg' called for an instance of LowHello" }
--- spec/fixtures/low_hello.rb
+++ spec/fixtures/low_hello.rb
@@ -70,25 +70,6 @@ class LowHello
     greetings
   end
 
-  # Return types.
-
-  def return_type() -> { Integer }
-    2 + 2
-  end
-
-  def array_return_type() -> { Array[Symbol] }
-    %i[one two three]
-  end
-
-  def arg_and_return_type(greeting) -> { String }
-    2 + 2
-    greeting
-  end
-
-  def arg_and_nilable_return_value(greeting) -> { String | nil }
-    greeting
-  end
-
   private
 
   def private_typed_arg(greeting = String)
