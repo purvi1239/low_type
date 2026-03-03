5a97700dbee60daea47bd9b3b7b4037056690bc3
maedi <maediprichard@gmail.com>
2026-03-03 23:57:26 +1100

    Comment out proxy factory, use for reference

--- spec/units/proxy_factory_spec.rb
+++ spec/units/proxy_factory_spec.rb
@@ -1,75 +1,75 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'prism'
-require_relative '../../lib/factories/proxy_factory'
+# require 'prism'
+# require_relative '../../lib/factories/proxy_factory'
 
-RSpec.describe Low::ProxyFactory do
-  describe '.return_proxy' do
-    let(:file_path) { '/path/to/test_class.rb' }
-    let(:name) { 'mock name' }
-    let(:scope) { 'TestClass#test_method' }
+# RSpec.describe Low::ProxyFactory do
+#   describe '.return_proxy' do
+#     let(:file_path) { '/path/to/test_class.rb' }
+#     let(:name) { 'mock name' }
+#     let(:scope) { 'TestClass#test_method' }
 
-    context 'with a valid return type' do
-      let(:method_code) do
-        <<~RUBY
-          def test_method() -> { String }
-            "hello"
-          end
-        RUBY
-      end
+#     context 'with a valid return type' do
+#       let(:method_code) do
+#         <<~RUBY
+#           def test_method() -> { String }
+#             "hello"
+#           end
+#         RUBY
+#       end
 
-      it 'creates a return proxy successfully' do
-        parsed = Prism.parse(method_code)
-        method_node = parsed.value.statements.body.first
+#       it 'creates a return proxy successfully' do
+#         parsed = Prism.parse(method_code)
+#         method_node = parsed.value.statements.body.first
 
-        expect { described_class.return_proxy(method_node:, name:, file_path:, scope:) }.not_to raise_error
-      end
+#         expect { described_class.return_proxy(method_node:, name:, file_path:, scope:) }.not_to raise_error
+#       end
 
-      it 'returns a ReturnProxy instance' do
-        parsed = Prism.parse(method_code)
-        method_node = parsed.value.statements.body.first
+#       it 'returns a ReturnProxy instance' do
+#         parsed = Prism.parse(method_code)
+#         method_node = parsed.value.statements.body.first
 
-        result = described_class.return_proxy(method_node:, name:, file_path:, scope:)
+#         result = described_class.return_proxy(method_node:, name:, file_path:, scope:)
 
-        expect(result).to be_a(Lowkey::ReturnProxy)
-      end
-    end
+#         expect(result).to be_a(Lowkey::ReturnProxy)
+#       end
+#     end
 
-    context 'with an unknown return type' do
-      let(:method_code) do
-        <<~RUBY
-          def test_method() -> { UnknownType }
-            "hello"
-          end
-        RUBY
-      end
+#     context 'with an unknown return type' do
+#       let(:method_code) do
+#         <<~RUBY
+#           def test_method() -> { UnknownType }
+#             "hello"
+#           end
+#         RUBY
+#       end
 
-      it 'raises NameError with improved error message' do
-        parsed = Prism.parse(method_code)
-        method_node = parsed.value.statements.body.first
+#       it 'raises NameError with improved error message' do
+#         parsed = Prism.parse(method_code)
+#         method_node = parsed.value.statements.body.first
 
-        expect { described_class.return_proxy(method_node:, name:, file_path:, scope:) }
-          .to raise_error(NameError, "Unknown return type '-> { UnknownType }' for TestClass#test_method at /path/to/test_class.rb:1")
-      end
-    end
+#         expect { described_class.return_proxy(method_node:, name:, file_path:, scope:) }
+#           .to raise_error(NameError, "Unknown return type '-> { UnknownType }' for TestClass#test_method at /path/to/test_class.rb:1")
+#       end
+#     end
 
-    context 'with no return type' do
-      let(:method_code) do
-        <<~RUBY
-          def test_method
-            "hello"
-          end
-        RUBY
-      end
+#     context 'with no return type' do
+#       let(:method_code) do
+#         <<~RUBY
+#           def test_method
+#             "hello"
+#           end
+#         RUBY
+#       end
 
-      it 'returns nil' do
-        parsed = Prism.parse(method_code)
-        method_node = parsed.value.statements.body.first
+#       it 'returns nil' do
+#         parsed = Prism.parse(method_code)
+#         method_node = parsed.value.statements.body.first
 
-        result = described_class.return_proxy(method_node:, name:, file_path:, scope:)
+#         result = described_class.return_proxy(method_node:, name:, file_path:, scope:)
 
-        expect(result).to be_nil
-      end
-    end
-  end
-end
+#         expect(result).to be_nil
+#       end
+#     end
+#   end
+# end
