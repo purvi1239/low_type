7b7183eea15057ee14abf474fa9de53bd7bcbc9a
maedi <maediprichard@gmail.com>
2026-03-20 23:41:37 +1100

    When inherited, file path is different to callee

--- lib/queries/file_query.rb
+++ lib/queries/file_query.rb
@@ -6,23 +6,11 @@ module Low
   class FileQuery
     class << self
       def file_path(klass:)
-        includer_line = line_from_class(klass:) || line_from_include || ''
-        file_path = includer_line.split(':').first || ''
+        file_path = Object.const_source_location(klass.name).first
 
         return file_path if File.exist?(file_path)
 
         raise MissingFileError, "No file found at path '#{file_path}'"
-      end
-
-      private
-
-      def line_from_class(klass:)
-        class_name = klass.to_s.split(':').last # Also remove the module namespaces from the class.
-        caller.find { |callee| callee.end_with?("<class:#{class_name}>'") }
-      end
-
-      def line_from_include
-        caller.find { |callee| callee.end_with?("include'") }
       end
     end
   end
